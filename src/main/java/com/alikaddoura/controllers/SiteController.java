2aeea39a8e0340aa192826bf7f2b65366e576893
alikaddoura1 <agakaddoura@gmail.com>
2024-05-28 13:51:52 -0700

    added stuff in html

--- src/main/java/com/alikaddoura/controllers/SiteController.java
+++ src/main/java/com/alikaddoura/controllers/SiteController.java
@@ -1,6 +1,7 @@
 package com.alikaddoura.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SiteController {
 	
 	@GetMapping("/home")
-	public String homePage() {
+	public String homePage(Model model) {
+		model.addAttribute("message", "have a greate day");
 		
 		return "home.html";
 	}
 }
+
--- src/main/java/com/alikaddoura/model/User.java
+++ src/main/java/com/alikaddoura/model/User.java
@@ -1,9 +1,32 @@
 package com.alikaddoura.model;
 
 public class User {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	
+	public User(int id, String firstName, String lastName, String userName, String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName= userName;
+		this.password = password;
+	
+	}
+	
+	
+		
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		User me = new User(1, "ali", "kaddoura","alikaddoura", "1100");
+		
+		System.out.println(me.firstName);
+			
+		
 
 	}
 
